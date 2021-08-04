41ff5473c3afe8ffa59e3e2ec02ae590be9e47b0
sean <tmddnr3163@gmail.com>
2021-08-04 23:06:35 +0900

    Feat: Eslint 반영

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useRef } from 'react';
 import styled from 'styled-components';
 import { Button, Title } from '../pages/admin/Admin.styles';
 
