7ce63a7b526191e6c7f4a650335f489903f45eb2
jamieecode <71512593+jamieecode@users.noreply.github.com>
2021-08-05 22:48:13 +0900

    Feature/login#40 스타일 적용 (#49)
    * login
    
    * feature/login styles

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,11 +1,9 @@
 import React, { useState, useCallback } from 'react';
 import LoginForm from './LoginForm';
 import { useHistory } from 'react-router';
-import { UserStorage } from '../../utils/userStorage';
 
 const LoginPage = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const currentUser = new UserStorage('currentUser');
   const history = useHistory();
 
   const submitForm = useCallback(() => {
@@ -17,13 +15,7 @@ const LoginPage = () => {
   return (
     <div>
       Hello LogInPage!
-      <div>
-        {!isSubmitted ? (
-          <LoginForm submitForm={submitForm} />
-        ) : (
-          <p>로그인성공</p> && currentUser.getUser().id
-        )}
-      </div>
+      <div>{!isSubmitted ? <LoginForm submitForm={submitForm} /> : <p>로그인성공</p>}</div>
     </div>
   );
 };
--- src/pages/login/LoginPageStyles.js
+++ src/pages/login/LoginPageStyles.js
@@ -1,11 +1,58 @@
 import styled from 'styled-components';
+
 export const LoginSection = styled.section`
-  border: 2px solid green;
   text-align: center;
+  margin: 3em auto 0;
+  width: 50%;
+  h1 {
+    font-size: 3rem;
+    font-weight: bold;
+    margin-bottom: 1em;
+  }
+
+  a {
+    text-decoration: none;
+    width: 100%;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  div {
+    width: 55%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto 0.5em;
+  }
+
+  label {
+    font-size: 1.1rem;
+  }
 `;
 
 export const Button = styled.button`
   border: none;
   padding: 0.5em 1em;
   border-radius: 6px;
+  background-color: #aac14f;
+  display: block;
+  margin: 0 auto;
+  color: #ffffff;
+  font-size: 20px;
+  margin-top: 20px;
+`;
+
+export const Input = styled.input`
+  padding: 1em;
+  border: 1px solid #c4c4c4;
+  border-radius: 6px;
+  text-align: center;
+  font-size: 1rem;
+`;
+
+export const ErrorDiv = styled.div`
+  height: 1.5rem;
 `;
